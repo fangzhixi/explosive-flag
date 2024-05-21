043954f658e566842fe209fdcfaf56e4b0b49158
root <fangzx@szszfw.gov.cn>
2024-05-21 16:10:49 +0800

    add reverse

---
+++ explosive_flag/main.c
@@ -0,0 +1,47 @@
+#include"explosive_flag.h"
+
+char data[] =
+        {
+                0x7E, 0x7D, 0x7C, 0x7B, 0x7A, 0x79, 0x78, 0x77, 0x76, 0x75,
+                0x74, 0x73, 0x72, 0x71, 0x70, 0x6F, 0x6E, 0x6D, 0x6C, 0x6B,
+                0x6A, 0x69, 0x68, 0x67, 0x66, 0x65, 0x64, 0x63, 0x62, 0x61,
+                0x60, 0x5F, 0x5E, 0x5D, 0x5C, 0x5B, 0x5A, 0x59, 0x58, 0x57,
+                0x56, 0x55, 0x54, 0x53, 0x52, 0x51, 0x50, 0x4F, 0x4E, 0x4D,
+                0x4C, 0x4B, 0x4A, 0x49, 0x48, 0x47, 0x46, 0x45, 0x44, 0x43,
+                0x42, 0x41, 0x40, 0x3F, 0x3E, 0x3D, 0x3C, 0x3B, 0x3A, 0x39,
+                0x38, 0x37, 0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30, 0x2F,
+                0x2E, 0x2D, 0x2C, 0x2B, 0x2A, 0x29, 0x28, 0x27, 0x26, 0x25,
+                0x24, 0x23, 0x20, 0x21, 0x22, 0x00
+        };
+
+int __cdecl main(int argc, const char **argv, const char **envp) {
+    char v4[12]; // [esp+12h] [ebp-2Eh] BYREF
+    char v5[12]; // [esp+1Eh] [ebp-22h]
+    char *input_str; // [esp+2Ah] [ebp-16h] BYREF
+    int i; // [esp+3Ch] [ebp-4h]
+
+    memcpy(v4, "*F'\"N,\"(I?+@", sizeof(v4));
+    printf("Please input:");
+//    scanf("%s", input_str);
+    input_str = GenerateFlagStart();
+//    setjmp(jump_buf);
+//    GenerateStrcmp(input_str,"ACTF{");
+    i = 0;
+    GenerateFlagPrefix("ACTF{");
+    GenerateFlagSuffix("}");
+    printf("%s", input_str);
+    setjmp(jump_buf);
+    strcpy(v5, input_str);
+    for (; i <= 11; ++i) {
+
+        if (v4[i] != data[v5[i + 5] - 1]) {
+            GenerateFlag(GENERATE_STATUS_IS_NOT_MATCH_CURRENT_VALUE, GENERATE_MATCH_NO_PARSE_LOOP_INDEX);
+        } else {
+            printf("\n ");
+            GenerateFlag(GENERATE_STATUS_IS_MATCH_CURRENT_VALUE, GENERATE_MATCH_NO_PARSE_LOOP_INDEX);
+        }
+    }
+    GenerateFlag(GENERATE_STATUS_FINISH, GENERATE_MATCH_NO_PARSE_LOOP_INDEX);
+    printf("You are correct!");
+    return 0;
+}
---
+++ explosive_flag/tools.c
@@ -0,0 +1,189 @@
+#include"explosive_flag.h"
+
+int flag_max_len = MAX_FLAG_LENGTH;
+char flag_str[MAX_FLAG_LENGTH] = {0};
+char flag_result[MAX_FLAG_LENGTH] = {0};
+char flag_suffix[MAX_FLAG_LENGTH / 8] = {0};
+
+jmp_buf jump_buf;
+
+/*
+ * 爆破强制跳转
+ * 需搭配 setjmp(jump_buf); 标记跳转节点
+ * 为确保服务健壮性, 跳转前需确认是否已经标记跳转节点
+ * */
+void generateJump() {
+    size_t buf_len = sizeof(jump_buf) / sizeof(jump_buf[0]);
+    for (int i = 0; i < buf_len; i++) {
+        if (jump_buf[i].Part[0] != EMPTY || jump_buf[i].Part[1] != EMPTY) {
+            break;
+        }
+        if (i == buf_len - 1) {
+            printf("\ntips: 貌似忘记设置返回点了\n");
+            printf("tips: 请使用 setjmp(jump_buf); 标记跳转点位\n");
+            printf("tips: setjmp(jump_buf); 推荐设置在程序输入flag附近, 请检查\n");
+            exit(0);
+        }
+    }
+    longjmp(jump_buf, 1);
+}
+
+/*
+ * 碰撞flag
+ * 使用场景为flag与目标数组逐一比对验证
+ * 原理为flag单个字符按顺序逐一碰撞, 实现效率最优
+ * 需要断点回溯逻辑放置setjmp(jump_buf);实现断点回溯
+ *
+ * param setjmp(jump_buf); 断点回溯
+ * param match_status 配对状态值
+ *      GENERATE_STATUS_CLEAR_VALUE 清空记录flag结果
+ *      GENERATE_STATUS_START 碰撞开始
+ *      GENERATE_STATUS_IS_NOT_MATCH_CURRENT_VALUE 未找到当前值
+ *      GENERATE_STATUS_IS_MATCH_CURRENT_VALUE 已找到当前值
+ *      GENERATE_STATUS_FINISH 碰撞结束
+ * param match_index 当前flag比对位置
+ *      GENERATE_MATCH_NO_PARSE_LOOP_INDEX 不分析for循环中flag正在比对的位置
+ * */
+char *GenerateFlag(int match_status, int match_index) {
+    int flag_match_len = (int) strlen(flag_str);
+    if (flag_match_len == 0) {
+        flag_str[0] = MIN_PRINT_ASCII;
+        strcpy(flag_result, flag_str);
+        return flag_result;
+    }
+
+    if (flag_match_len > flag_max_len || flag_str[flag_max_len - 1] == 0x7f) {
+        printf("\n测试用例结束，未找到目标flag: %d\n", flag_match_len);
+        exit(0);
+    }
+
+    char ascii;
+    switch (match_status) {
+        case GENERATE_STATUS_IS_MATCH_CURRENT_VALUE:
+            if (match_index >= 0 && flag_match_len != match_index + 1) {
+                return flag_result;
+            }
+            printf("%s\n", flag_str);
+            flag_str[flag_match_len] = MIN_PRINT_ASCII;
+            strcpy(flag_result, flag_str);
+            return flag_result;
+
+        case GENERATE_STATUS_IS_NOT_MATCH_CURRENT_VALUE:
+            //递归终点
+            if (flag_str[0] == MAX_PRINT_ASCII) {
+                printf("\n测试用例存在缺陷, 第%d位已完成全部字符遍历,但未碰撞出目标值\n", flag_match_len);
+                printf("结果: %s\n", flag_str);
+                printf("请检查放置goto语句[setjmp(jump_buf);]是否正确, 或本题不适用单字符碰撞\n");
+                exit(0);
+            }
+            for (int i = 1; i < flag_match_len; i++) {
+                ascii = flag_str[i];
+                //递归起点
+                if (ascii == MAX_PRINT_ASCII) {
+                    //回滚，假设上一个字符正确值不唯一
+                    flag_match_len = i;
+                    flag_str[flag_match_len - 1] += 1;
+                    flag_str[flag_match_len] = '\0';//清空当前字符
+                    strcpy(flag_result, flag_str);
+                    generateJump();
+                }
+            }
+
+            flag_str[flag_match_len - 1] += 1;
+            strcpy(flag_result, flag_str);
+            generateJump();
+
+        case GENERATE_STATUS_FINISH:
+            printf("\n碰撞结束!\n结果: %s%s\n", flag_str, flag_suffix);
+            return flag_result;
+
+        case GENERATE_STATUS_CLEAR_VALUE:
+            memset(flag_str, '\0', MAX_FLAG_LENGTH);
+            strcpy(flag_result, flag_str);
+            return flag_result;
+
+        default:
+            printf("\n未知状态码: %d\n", match_status);
+            exit(0);
+    }
+}
+
+/*
+ * 增加flag前缀
+ * 手动增加前缀不影响flag碰撞结果,可以重复追加前缀
+ * */
+bool GenerateFlagPrefix(char *prefix_str) {
+    if (strlen(flag_str) + strlen(prefix_str) > MAX_FLAG_LENGTH / 2) {
+        printf("flag前缀超出额定长度");
+        return false;
+    }
+
+    int prefix_str_len = (int) strlen(prefix_str);
+    for (int i = (int) strlen(flag_str) - 1; i >= 0; i--) {
+        flag_str[i + prefix_str_len] = flag_str[i];
+    }
+
+    for (int i = 0; i < prefix_str_len; i++) {
+        flag_str[i] = prefix_str[i];
+    }
+    strcpy(flag_result, flag_str);
+    return true;
+}
+
+/*
+ * 增加flag后缀
+ * 手动增加后缀不影响flag碰撞结果
+ * */
+bool GenerateFlagSuffix(char *suffix_str) {
+    strcpy(flag_suffix, suffix_str);
+    return true;
+}
+
+/*
+ *  验证碰撞flag值，平替strcmp
+ *  需要与断点回溯配合使用 setjmp(jump_buf);
+ *  此方法对三种可能性进行处理
+ *      ①当前字符与目标值不一致时，提供新的字符并强制跳转到标记点参与新一次碰撞
+ *      ②当前字符与目标值一致时，记录当前字符并寻找下一个字符
+ *      ③碰撞字符串与目标值完成一致时，碰撞成功，输出结果
+ *  param generate_str 碰撞字符串
+ *  param target_str 目标字符串(flag等)
+ * */
+int GenerateStrcmp(char *generate_str, char *target_str) {
+    int is_success = true;
+    int str_len_max = (int) strlen(target_str);
+    int str_len_min = (int) strlen(generate_str);
+
+    if ((int) strlen(generate_str) > str_len_max) {
+        str_len_min = (int) strlen(target_str);
+        str_len_max = (int) strlen(generate_str);
+    }
+
+    if (str_len_min == 0) {
+        printf("\ntips: 碰撞字符串可能未设置正确, 推荐设置在程序输入flag处, 请检查\n");
+        system("PAUSE");
+    }
+
+    for (int i = 0; i < str_len_max; i++) {
+        if (generate_str[i] == target_str[i]) {
+            //碰撞成功
+            GenerateFlag(GENERATE_STATUS_IS_MATCH_CURRENT_VALUE, i);
+        } else {
+            //碰撞失败
+            GenerateFlag(GENERATE_STATUS_IS_NOT_MATCH_CURRENT_VALUE, i);
+            is_success = false;
+        }
+    }
+    GenerateFlag(GENERATE_STATUS_FINISH, GENERATE_MATCH_NO_PARSE_LOOP_INDEX);
+    return is_success;
+}
+
+/*
+ * 碰撞flag
+ * 使用场景为flag与目标数组逐一比对验证
+ * 原理为flag单个字符按顺序逐一碰撞, 实现效率最优
+ * 需要断点回溯逻辑放置setjmp(jump_buf);实现断点回溯
+ * */
+char *GenerateFlagStart() {
+    return GenerateFlag(GENERATE_STATUS_START, GENERATE_MATCH_NO_PARSE_LOOP_INDEX);
+}
